b3f40c0da516708ee7db69dc307454a57548bda4
zuk2y <suz.liang.oji@gmail.com>
2020-04-26 16:52:45 +0900

    テンプレート追加

---
+++ src/template/a/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(final String[] args) {
+        final Scanner sc = new Scanner(System.in);
+        
+        System.out.println();
+    }
+}
